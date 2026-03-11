5582882c83bcf3b5c89b848e2785c29285c2b18f
Bernardo O. Horta <bernardo.o.horta@gmail.com>
2026-03-11 08:51:37 -0300

    1.1.1
    Fix data on ModHelperData

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace HeroesBuff;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "52.0";
-    public const string Version = "1.0.0";
+    public const string WorksOnVersion = "53.0";
+    public const string Version = "1.1.0";
     public const string Name = "HeroesBuff";
 
     public const string Description = """
